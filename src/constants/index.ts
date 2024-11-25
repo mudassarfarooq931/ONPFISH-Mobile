30787d8dfe6a21559cabfe76c2e4ff7bd77e7123
mudassarfarooq1918 <mudassar@hawklogix.com>
2024-11-25 15:01:54 +0500

    code-fixes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,6 @@
 import * as colors from './colors';
 import * as endpoints from './endpoints';
 import * as fonts from './fonts';
-
 import {RolesEnum} from './roles';
 import {ScreenEnum} from './screen-name';
 import strings from './strings';
